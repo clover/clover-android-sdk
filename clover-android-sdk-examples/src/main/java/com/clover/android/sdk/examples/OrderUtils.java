72664a7d1c51076b857054982ed2a99557ef0dfe
Mike Brooking <mike.brooking@clover.com>
2024-03-06 10:36:56 -0700

    Updated for MSC

--- clover-android-sdk-examples/src/main/java/com/clover/android/sdk/examples/OrderUtils.java
+++ clover-android-sdk-examples/src/main/java/com/clover/android/sdk/examples/OrderUtils.java
@@ -1,9 +1,14 @@
 package com.clover.android.sdk.examples;
 
+import com.clover.sdk.v3.inventory.OrderFee;
+import com.clover.sdk.v3.order.LineItem;
 import com.clover.sdk.v3.order.Order;
 import com.clover.sdk.v3.order.OrderCalc;
 import com.clover.sdk.v3.payments.Payment;
 import com.clover.sdk.v3.payments.Refund;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class OrderUtils {
 
@@ -51,6 +56,53 @@ public final class OrderUtils {
     } else {
       return 0;
     }
+  }
+
+  /**
+   * This method filters out service charge line items and returns the list of regular line items
+   * associated with the given order
+   *
+   * @param order: order instance whose regular line items are to be obtained
+   * @return list of regular line items without service charge line items
+   */
+  public static List<LineItem> getLineItemsWithoutServiceCharges(Order order) {
+    return getLineItemsWithoutServiceCharges(order.getLineItems());
+  }
+
+  /**
+   * This method filters out service charge line items and returns the list of regular line items
+   * from a given list of lineitems
+   *
+   * @param lineItems: list of line items from which the service charge line items should be filtered out
+   * @return list of regular line items without service charge line items
+   */
+  public static List<LineItem> getLineItemsWithoutServiceCharges(List<LineItem> lineItems) {
+    List<LineItem> lineItemsWithoutServiceCharges = null;
+    if (lineItems != null) {
+      for (LineItem lineItem : lineItems) {
+        if (!lineItem.getIsOrderFee()) {
+          lineItemsWithoutServiceCharges.add(lineItem);
+        }
+      }
+    }
+    return lineItemsWithoutServiceCharges;
+  }
+
+  /**
+   * Returns the list of service charge(s) which are applied to current order
+   */
+  public static List<LineItem> getServiceChargesFromOrder(Order order) {
+    List<LineItem> serviceCharges = new ArrayList<LineItem>();
+    if (order.isNotNullLineItems()) {
+      for (LineItem lineItem : order.getLineItems()) {
+        if (lineItem.isNotNullIsOrderFee() && lineItem.getIsOrderFee()) {
+          serviceCharges.add(
+             lineItem
+          );
+        }
+      }
+    }
+    return serviceCharges;
   }
 
 }
